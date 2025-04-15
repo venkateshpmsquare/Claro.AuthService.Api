ccf8e23896ec7384f6042f3ba58a392a455606ee
FrontEnd Developer <fedeveloper1@outlook.com>
2025-04-15 21:12:06 +0530

    git ignore changes.

---
+++ Claro.AuthService.Api/Program.cs
@@ -0,0 +1,100 @@
+using Claro.AuthService.Application.Helpers;
+using Claro.AuthService.Application.Interfaces;
+using Claro.AuthService.Application.Services;
+using Claro.AuthService.Infrastructure.Persistence;
+using Claro.AuthService.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//add swagger service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]);
+
+// Add Identity services 
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//                .AddEntityFrameworkStores<ClaroAuthDbContext>()
+//                .AddDefaultTokenProviders();
+
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddDbContext<ClaroAuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+builder.Services.AddSingleton<PasswordHasher>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<Claro.AuthService.Infrastructure.Interfaces.IUserRepository, UserRepository>();  
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+// Add CORS Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowClaroUI",
+        policy => policy.WithOrigins("http://localhost:4200") // Update to your Angular domain
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+// Use CORS
+app.UseCors("AllowClaroUI");
+
+app.UseSwagger();
+    app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+app.MapOpenApi();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
